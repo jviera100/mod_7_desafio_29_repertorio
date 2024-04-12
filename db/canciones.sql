810774703ffbbb6e97833a9a5c315719235485a5
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-04-12 18:58:19 -0400

    :data: funciona

--- db/canciones.sql
+++ db/canciones.sql
@@ -1,3 +1,4 @@
 CREATE TABLE canciones (id SERIAL, titulo VARCHAR(50), artista 
 VARCHAR(50), tono VARCHAR(10)); 
 select * from canciones;
+insert into canciones(titulo, artista, tono) values('Brian May', 'Java', 'Basico');
