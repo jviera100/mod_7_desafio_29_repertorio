17b553037ce38637431bcef3e521e45109c00343
Guido Perez <gperzal@gmail.com>
2024-04-14 22:58:32 -0400

    :sparkles: Fix Error Edit + Delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,129 @@
- // import axios from "/node_modules/axios/dist/axios.min.js";
-  
-  // let url = "/";
-  let tbody = document.getElementById("cuerpo");
+
+let tbody = document.getElementById("cuerpo");
 
 
-  let canciones = [];
-  window.onload = obtenerData(); //al cargar la pagina se obtienen los datos
+let canciones = [];
+window.onload = obtenerData(); //al cargar la pagina se obtienen los datos
 
 // funcion obtenerData se encarga de estructura de la tabla lista de canciones
-  async function obtenerData() {
-    await axios.get("/obtenerData").then((data) => { //obtiene datos de la db y se almacenan en data
-      canciones = data.data; //variable canciones almacena los datos de variable data
-      tbody.innerHTML = ""; //inner cambia lo que hay dentro por el string vacio
-      console.log(canciones)
-      canciones.forEach((c, i) => { //array de objetos canciones, c = cancion, i = indice
-        tbody.innerHTML += `
+async function obtenerData() {
+  await axios.get("/obtenerData").then((data) => { //obtiene datos de la db y se almacenan en data
+    canciones = data.data; //variable canciones almacena los datos de variable data
+    tbody.innerHTML = ""; //inner cambia lo que hay dentro por el string vacio
+    canciones.forEach((c, i) => { //array de objetos canciones, c = cancion, i = indice
+      tbody.innerHTML += `
         <tr>
           <td>${i + 1}</td>
-          <td>${c.cancion}</td>
+          <td>${c.titulo}</td>
           <td>${c.artista}</td>
           <td>${c.tono}</td>
           <td>
-          <button class="btn btn-warning" data-index="${i}" data-id="${c.id}">Editar</button>
-          <button class="btn btn-danger" data-index="${i}" data-id="${c.id}">Eliminar</button>
+            <button class="btn btn-warning" data-index="${i}" data-id="${c.id}">Editar</button>
+            <button class="btn btn-danger" data-index="${i}" data-id="${c.id}">Eliminar</button>
           </td>
         </tr>
       `;
       //botones de editar y eliminar dentro de la tabla lista de canciones
-      });
     });
-    // Limpia los campos de entrada después de obtener los datos
-    cancion.value = "";
-    artista.value = "";
-    tono.value = "";
-  }
+  });
+  // Limpia los campos de entrada después de obtener los datos
+  cancion.value = "";
+  artista.value = "";
+  tono.value = "";
+}
 // Agrega un listener a todos los botones de editar dentro de tabla
-  document.querySelectorAll('.btn-warning').forEach(btn => { //seleciona boton editar dentro de tabla
-    btn.addEventListener('click', () => editarData(btn.dataset.index, btn.dataset.id)); //edita indice y id señalado
-  });
-  // Agrega un listener a todos los botones de eliminar dentro de tabla
-  document.querySelectorAll('.btn-danger').forEach(btn => { //seleciona boton eliminar dentro de tabla
-    btn.addEventListener('click', () => eliminarData(btn.dataset.index, btn.dataset.id)); //elimina indice y id senalado
-  });
-  // Agrega un listener a todos los botones de registrar dentro de formulario
-  document.querySelectorAll('.btn-registrar').forEach(btn => btn.addEventListener("click", registrarData));
+document.querySelectorAll('.btn-warning').forEach(btn => {
+  btn.addEventListener('click', (e) => editarData(btn.dataset.index, btn.dataset.id, e));
+});
 
-  // Función para registrar una nueva canción
-  function registrarData() {
-    const cancion = document.getElementById("cancion").value;
-    const artista = document.getElementById("artista").value;
-    const tono = document.getElementById("tono").value;
-    // const data = {
-    //   titulo: titulo.value,
-    //   artista: artista.value,
-    //   tono: tono.value,
-    // };
-    console.log(cancion, artista, tono);
-     // Realiza una solicitud POST para registrar la canción
-    axios.post("/registrarData", {cancion, artista, tono}).then(() => obtenerData());
-  }
+document.querySelectorAll('.btn-danger').forEach(btn => {
+  btn.addEventListener('click', () => eliminarData(btn.dataset.index, btn.dataset.id));
+});
+// Agrega un listener a todos los botones de registrar dentro de formulario
+document.querySelectorAll('.btn-registrar').forEach(btn => btn.addEventListener("click", registrarData));
+
+// Función para registrar una nueva canción
+function registrarData() {
+  const titulo = document.getElementById("cancion").value;
+  const artista = document.getElementById("artista").value;
+  const tono = document.getElementById("tono").value;
+
+  console.log(titulo, artista, tono);
+  // Realiza una solicitud POST para registrar la canción
+  axios.post("/registrarData", { titulo, artista, tono }).then(() => obtenerData());
+}
 // Función para eliminar una canción
-  function eliminarData(i, id) {
-    console.log(i, id);
-    axios.delete("/eliminarData/ " + id).then(() => {
-      alert("Canción " + canciones[i].cancion + " eliminada");
-      obtenerData();
-    });
+function eliminarData(i, id, e) {
+  console.log(i, id, "eliminar");
+  e.stopPropagation();
+  console.log(i, id);
+  axios.delete("/eliminarData/ " + id).then(() => {
+    alert("Canción " + canciones[i].cancion + " eliminada");
+    obtenerData();
+  });
+}
+
+function editarData(i, id) {
+
+  const titulo = document.getElementById("cancion");
+  const artista = document.getElementById("artista");
+  const tono = document.getElementById("tono");
+  const botonEditar = document.getElementById("editar");
+
+  // Llena los campos de entrada con los datos de la canción seleccionada
+  titulo.value = canciones[i].titulo;  // Asegúrate que 'titulo' es la propiedad correcta
+  artista.value = canciones[i].artista;
+  tono.value = canciones[i].tono;
+
+  // Configura el botón de editar para llamar a la función actualizarData con el ID de la canción
+  botonEditar.onclick = function () { actualizarData(id); };
+
+  // Oculta el botón de agregar y muestra el botón de editar
+  document.getElementById("agregar").style.display = "none";
+  botonEditar.style.display = "block";
+}
+
+function actualizarData(id) {
+  const titulo = document.getElementById("cancion").value;
+  const artista = document.getElementById("artista").value;
+  const tono = document.getElementById("tono").value;
+  console.log(titulo, artista, tono);
+  axios.put(`/actualizarData/${id}`, {
+    titulo,
+    artista,
+    tono
+  }).then(() => {
+    alert('Canción actualizada con éxito');
+    obtenerData();  // Actualiza la tabla para mostrar los cambios
+    document.getElementById("agregar").style.display = "block";
+    document.getElementById("editar").style.display = "none";
+  }).catch(error => {
+    console.error('Error al actualizar la canción:', error);
+    alert('Error al actualizar la canción');
+  });
+}
+
+
+// Esta función se coloca fuera de cualquier otra función para asegurarse de que solo se establece una vez
+document.getElementById("cuerpo").addEventListener("click", function (e) {
+  // Comprueba si el elemento clicado es un botón de editar
+  if (e.target && e.target.matches(".btn-warning")) {
+    let btn = e.target;
+    editarData(btn.dataset.index, btn.dataset.id);
   }
-// Función para editar una canción
-   function editarData(i, id) {
-    // Llena los campos de entrada con los datos de la canción seleccionada
-     cancion.value = canciones[i].cancion;
-    artista.value = canciones[i].artista;
-    tono.value = canciones[i].tono;
-    // Configura el botón de editar para llamar a la función actualizarData con el ID de la canción
-     document
-      .getElementById("editar")
-       .setAttribute("onclick", `editarCancion('${id}')`);
-    // Oculta el botón de agregar y muestra el botón de editar
-     document.getElementById("agregar").style.display = "none";
-     document.getElementById("editar").style.display = "block";
-   }
-// Función para actualizar una canción
-   function actualizarData(id) {
-    // Realiza una solicitud PUT para actualizar la canción con el ID proporcionado
-    axios
-      .put(`/actualizarData/${id}`, { // Utiliza la ruta correcta con el ID
-        titulo: cancion.value,
-        artista: artista.value,
-        tono: tono.value,
-      })
-      .then(() => {
-        obtenerData(); // Llama a la función para actualizar los datos después de la edición
-        document.getElementById("agregar").style.display = "block";
-        document.getElementById("editar").style.display = "none";
-      })
-      .catch((error) => {
-        console.error('Error al actualizar la canción:', error);
-        // Manejar el error si es necesario
-      });
+
+  // Comprueba si el elemento clicado es un botón de eliminar
+  if (e.target && e.target.matches(".btn-danger")) {
+    let btn = e.target;
+    eliminarData(btn.dataset.index, btn.dataset.id);
   }
-// Agrega un listener para detectar clics en cualquier botón dentro del tbody
-tbody.addEventListener("click", (e) => { //e = evento
-  if (e.target.classList.contains("btn")) { // verifica si el elemento clickeado tiene la clase btn
-    let id = e.target.getAttribute("data-id"); //obtiene el id del elemento clickeado
+});
+
+
+tbody.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-danger")) {
+    let id = e.target.getAttribute("data-id");
     console.log(id);
-     axios.delete("/eliminarData/" + id).then(() => obtenerData()); //elimina el elemento clickeado   
-  }});
+    axios.delete("/eliminarData/" + id).then(() => obtenerData());
+  }
+});
--- controllers/queriesDbPool.js
+++ controllers/queriesDbPool.js
@@ -4,24 +4,19 @@ import pool from "../config/dbPool.js"; // Importamos la conexión a la base de 
 export const registrarData = async (req, res) => {
   const { titulo, artista, tono } = req.body;
   try {
-    // Define el objeto queryObj con la consulta SQL y los valores dentro del try
     const queryObjAdd = {
       text: 'insert into canciones (titulo, artista, tono) values ($1,$2,$3) returning *',
       values: [titulo, artista, tono]
     };
     console.log(queryObjAdd);
-    
-    // Ejecuta la consulta utilizando el objeto queryObj
     const result = await pool.query(queryObjAdd);
     console.log('registrado exitosamente.');
-    // Retorna el resultado si es necesario
     res.json(result.rows);
-    // await pool.end(); => ya no es necesario, el pool se cierra automaticamente, 
-    //por lo que no es necesario cerrarlo
+
   } catch (error) {
-    // Captura y maneja cualquier error ocurrido durante la ejecución de la consulta
+
     console.error('Error al registrar data:', error.stack);
-    // Lanza el error para que sea manejado por el código que llame a esta función
+
     throw error;
   }
 };
@@ -31,76 +26,79 @@ export const registrarData = async (req, res) => {
 export const obtenerDataPorFiltro = async (req, res) => {
   const { id } = req.params;
   try {
-      const queryObjGetFilter = {
-          text: 'SELECT * FROM canciones WHERE id = $1',
-          values: [id]
-      };
-      const result = await pool.query(queryObjGetFilter);
-      console.log(result.rows);
-      console.log("Encontrado");
-      res.json(result.rows);
+    const queryObjGetFilter = {
+      text: 'SELECT * FROM canciones WHERE id = $1',
+      values: [id]
+    };
+    const result = await pool.query(queryObjGetFilter);
+    console.log(result.rows);
+    console.log("Encontrado");
+    res.json(result.rows);
   } catch (error) {
-      console.error('Error al obtener data por filtro:', error.stack);
-      throw error;
+    console.error('Error al obtener data por filtro:', error.stack);
+    throw error;
   }
 }
 
 // Función asíncrona para obtener por consola toda la data registrada
 export const obtenerData = async (req, res) => {
   try {
-      const queryObjGets = {
-          text: 'SELECT * FROM canciones'
-      };
-      const result = await pool.query(queryObjGets);
-      console.log(result.rows);
-      console.log("Encontrados");
-      res.json(result.rows);
+    const queryObjGets = {
+      text: 'SELECT * FROM canciones'
+    };
+    const result = await pool.query(queryObjGets);
+    console.log(result.rows);
+    res.json(result.rows);
   } catch (error) {
-      console.error('Error al obtener data:', error.stack);
-      throw error;
+    console.error('Error al obtener data:', error.stack);
+    throw error;
   }
 }
 
 // Función asíncrona para actualizar la data en la base de datos
-export const actualizarData = async (cancion) => {
+export const actualizarData = async (req, res) => {
+  const { titulo, artista, tono } = req.body;
+  const { id } = req.params;
   try {
-      const queryObjUpdate = {
-          text: 'UPDATE canciones SET titulo = $1, artista = $2, tono = $3 WHERE id = $4 RETURNING *',
-          values: [cancion]
-      };
-      const result = await pool.query(queryObjUpdate);
-      if (result.rows.length > 0) {
-          console.log(`Cancion con ${titulo} con artista ${artista} ha sido actualizado.`);
-      } else {
-          console.log('No se encontró cancion con el titulo proporcionado para actualizar.');
-      }
-      return result.rows;
+    const queryObjUpdate = {
+      text: 'UPDATE canciones SET titulo = $1, artista = $2, tono = $3 WHERE id = $4 RETURNING *',
+      values: [titulo, artista, tono, id] // Pasa todos los parámetros necesarios
+    };
+
+    const result = await pool.query(queryObjUpdate);
+    if (result.rows.length > 0) {
+      console.log(`Canción con ID ${id} ha sido actualizada.`);
+      res.json(result.rows[0]); // Envía la canción actualizada como respuesta
+    } else {
+      console.log('No se encontró canción con el ID proporcionado para actualizar.');
+      res.status(404).send('Canción no encontrada'); // Envía una respuesta de error
+    }
   } catch (error) {
-      console.error('Error al actualizar la cancion:', error.stack);
-      throw error;
+    console.error('Error al actualizar la canción:', error.stack);
+    res.status(500).send('Error al actualizar la canción'); // Envía una respuesta de error
   }
-}
+};
 
 // Función asíncrona para eliminar la data de la base de datos
 export const eliminarData = async (req, res) => {
   const { id } = req.params;
   try {
-      const queryObjDelete = {
-          text: 'DELETE FROM canciones WHERE id = $1',
-          values: [id]
-      };
-      const result = await pool.query(queryObjDelete);
-       //Delete requiere validación
-      // if (result.rows.length > 0) {
-      //     console.log(`Registros de canciones con titulo ${titulo} ha sido eliminado.`);
-      //     console.log(result.rows);
-      // } else {
-      //     console.log('No se encontró una cancion con el titulo proporcionado para eliminar.');
-      //     console.log(result.rows);
-      // }
-      res.json(result.rows);
+    const queryObjDelete = {
+      text: 'DELETE FROM canciones WHERE id = $1',
+      values: [id]
+    };
+    const result = await pool.query(queryObjDelete);
+    //Delete requiere validación
+    // if (result.rows.length > 0) {
+    //     console.log(`Registros de canciones con titulo ${titulo} ha sido eliminado.`);
+    //     console.log(result.rows);
+    // } else {
+    //     console.log('No se encontró una cancion con el titulo proporcionado para eliminar.');
+    //     console.log(result.rows);
+    // }
+    res.json(result.rows);
   } catch (error) {
-      console.error('Error al eliminar la cancion:', error.stack);
-      throw error;
+    console.error('Error al eliminar la cancion:', error.stack);
+    throw error;
   }
 }
